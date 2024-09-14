9f0b71e8b22a9eb7177097b98387e3a3ab1b4306
Harry <hariharasudhan2212@gmail.com>
2024-09-14 13:32:55 +0530

    footerupdate1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ function Footer() {
     <footer>
         
             <ul className='proicon'>
-                <li><a href="https://www.linkedin.com/in/harihara-sudan-78266b268?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><FaGithub /></a></li>
-                <li><a href="https://www.linkedin.com/in/harihara-sudan-78266b268?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><FaLinkedin /></a></li>
+                <li><a href="https://github.com/Hari-hara-sudan"><FaGithub /></a></li>
+                <li><a href="https://linkedin.com/in/harihara-sudan-78266b268"><FaLinkedin /></a></li>
             </ul>
       <p>© 2024 Hariharasudhan</p>
     </footer>
